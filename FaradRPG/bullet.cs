cd82c462701bcdb274d5b2ba5374d1dd29016cfa
Groophy-Lifefor <77299279+Groophy-Lifefor@users.noreply.github.com>
2021-06-18 13:09:05 +0300

    Add files via upload

---
+++ FaradRPG/bullet.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Windows.Forms;
+namespace FaradRPG
+{
+    class bullet
+    {
+        public string direction; // creating a public string called direction
+        public int speed = 20; // creating a integer called speed and assigning a value of 20
+        public int spead = 500;
+        PictureBox Bullet = new PictureBox(); // create a picture box 
+        Timer tm = new Timer(); // create a new timer called tm. 
+        
+
+        public int bulletLeft; // create a new public integer
+        public int bulletTop; // create a new public integer
+        int blloc;
+        int limit=0;
+        
+        public void mkkbullet(dungeon form)
+        {
+            if (direction == "up")
+            {
+                limit = blloc - 100;
+            }
+            if (direction == "down")
+            {
+                limit = blloc + 100;
+            }
+            if (direction == "left")
+            {
+                limit = blloc - 100;
+            }
+            if (direction == "right")
+            {
+                limit = blloc + 100;
+            }
+
+            //MessageBox.Show(blloc.ToString());
+            Bullet.BackColor = System.Drawing.Color.Transparent;
+            /*Bullet.BringToFront();
+            Bullet.Image = Properties.Resources.attackeffect;*/
+            //Bullet.BackgroundImage = Properties.Resources.attackeffect;
+            Bullet.Size = new Size(50, 50); // set the size to the bullet to 5 pixel by 5 pixel
+            Bullet.Tag = "bullet"; // set the tag to bullet
+            Bullet.Left = bulletLeft; // set bullet left 
+            Bullet.Top = bulletTop; // set bullet right
+            Bullet.BringToFront(); // bring the bullet to front of other objects
+            form.Controls.Add(Bullet); // add the bullet to the screen
+
+            tm.Interval = speed; // set the timer interval to speed
+            tm.Tick += new EventHandler(tm_Tick); // assignment the timer with an event
+            tm.Start(); // start the timer
+
+        }
+
+        public void mkBullet(Gamemain form)
+
+        {
+            if (direction=="up")
+            {
+                limit = blloc - 100;
+            }
+            if (direction == "down")
+            {
+                limit = blloc + 100;
+            }
+            if (direction == "left")
+            {
+                limit = blloc - 100;
+            }
+            if (direction == "right")
+            {
+                limit = blloc + 100;
+            }
+            
+            //MessageBox.Show(blloc.ToString());
+            Bullet.BackColor = System.Drawing.Color.Transparent; 
+            /*Bullet.BringToFront();
+            Bullet.Image = Properties.Resources.attackeffect;*/
+            //Bullet.BackgroundImage = Properties.Resources.attackeffect;
+            Bullet.Size = new Size(50, 50); // set the size to the bullet to 5 pixel by 5 pixel
+            Bullet.Tag = "bullet"; // set the tag to bullet
+            Bullet.Left = bulletLeft; // set bullet left 
+            Bullet.Top = bulletTop; // set bullet right
+            Bullet.BringToFront(); // bring the bullet to front of other objects
+            form.Controls.Add(Bullet); // add the bullet to the screen
+
+            tm.Interval = speed; // set the timer interval to speed
+            tm.Tick += new EventHandler(tm_Tick); // assignment the timer with an event
+            tm.Start(); // start the timer
+
+            
+        }
+
+        public void deneme(int bloc)
+        {
+            blloc = bloc;
+        }
+
+        
+
+        public void tm_Tick(object sender, EventArgs e)
+        {
+            // if direction equals to left
+            if (direction == "left")
+            {
+                Bullet.Left -= speed; // move bullet towards the left of the screen
+            }
+            // if direction equals right
+            if (direction == "right")
+            {
+                Bullet.Left += speed; // move bullet towards the right of the screen
+            }
+            // if direction is up
+            if (direction == "up")
+            {
+                Bullet.Top -= speed; // move the bullet towards top of the screen
+            }
+            // if direction is down
+            if (direction == "down")
+            {
+                Bullet.Top += speed; // move the bullet bottom of the screen
+            }
+
+            
+            
+            if (direction == "up" && Bullet.Location.Y <= limit )
+            {
+                tm.Stop(); // stop the timer
+                tm.Dispose(); // dispose the timer event and component from the program
+                Bullet.Dispose(); // dispose the bullet
+                tm = null; // nullify the timer object
+                Bullet = null; // nullify the bullet object
+            }
+            if (direction == "down" && Bullet.Location.Y >= limit)
+            {
+                tm.Stop(); // stop the timer
+                tm.Dispose(); // dispose the timer event and component from the program
+                Bullet.Dispose(); // dispose the bullet
+                tm = null; // nullify the timer object
+                Bullet = null; // nullify the bullet object
+            }
+            if (direction == "left" && Bullet.Location.X <= limit)
+            {
+                tm.Stop(); // stop the timer
+                tm.Dispose(); // dispose the timer event and component from the program
+                Bullet.Dispose(); // dispose the bullet
+                tm = null; // nullify the timer object
+                Bullet = null; // nullify the bullet object
+            }
+            if (direction == "right" && Bullet.Location.X >= limit)
+            {
+                tm.Stop(); // stop the timer
+                tm.Dispose(); // dispose the timer event and component from the program
+                Bullet.Dispose(); // dispose the bullet
+                tm = null; // nullify the timer object
+                Bullet = null; // nullify the bullet object
+            }
+        }
+
+
+
+        
+    }
+}
---
+++ FaradRPG/mainform.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+using System.IO;
+using System.IO.MemoryMappedFiles;
+using System.Runtime.InteropServices;
+using System.Media;
+
+namespace FaradRPG
+{
+    public partial class mainform : Form
+    {
+        public mainform()
+        {
+            InitializeComponent();
+        }
+        int pngtime = 0;
+        
+        public static string comed;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Gamemain gamemains = new Gamemain();
+            gamemains.Show();
+            this.Hide();
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnexit_MouseHover(object sender, EventArgs e)
+        {
+            btnexit.ForeColor = System.Drawing.Color.Red;
+        }
+
+        private void btnexit_MouseLeave(object sender, EventArgs e)
+        {
+            btnexit.ForeColor = System.Drawing.Color.Black;
+        }
+
+        private void btnoptions_MouseHover(object sender, EventArgs e)
+        {
+            btnoptions.ForeColor = System.Drawing.Color.Red;
+        }
+
+        private void btnoptions_MouseLeave(object sender, EventArgs e)
+        {
+            btnoptions.ForeColor = System.Drawing.Color.Black;
+        }
+
+        private void btnstart_MouseHover(object sender, EventArgs e)
+        {
+            btnstart.ForeColor = System.Drawing.Color.Red;
+        }
+
+        private void btnstart_MouseLeave(object sender, EventArgs e)
+        {
+            btnstart.ForeColor = System.Drawing.Color.Black;
+        }
+
+        private void btnoptions_Click(object sender, EventArgs e)
+        {
+            Options options = new Options();
+            options.Show();
+        }
+
+        private void mainform_Load(object sender, EventArgs e)
+        {
+            
+            if (Program.sng==false)
+            {
+                song("start");
+                Program.sng = true;
+            }
+
+            try
+            {
+                StreamReader read = new StreamReader("faradrpgdata.txt");
+                
+                read.Close();
+            }
+            catch
+            {
+                StreamWriter sw = new StreamWriter("faradrpgdata.txt");
+                sw.Close();
+            }
+        }
+
+        private void mainform_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Gamemain gm = new Gamemain();
+            StreamWriter sws = new StreamWriter("faradrpgdata.txt");
+            sws.Write("");
+            sws.WriteLine(gm.score);
+            sws.Close();
+        }
+
+        private void mainform_KeyDown(object sender, KeyEventArgs e)
+        {
+            /*if (e.KeyCode == Keys.Down)
+            {
+                if (btnstart.ForeColor == System.Drawing.Color.Red)
+                {
+                    btnstart.ForeColor = System.Drawing.Color.Black;
+                    btnoptions.ForeColor = System.Drawing.Color.Red;
+                }
+                if (btnoptions.ForeColor == System.Drawing.Color.Red)
+                {
+                    btnoptions.ForeColor = System.Drawing.Color.Black;
+                    btnexit.ForeColor = System.Drawing.Color.Red;
+                }
+                if (btnexit.ForeColor == System.Drawing.Color.Red)
+                {
+                    btnexit.ForeColor = System.Drawing.Color.Black;
+                    btnstart.ForeColor = System.Drawing.Color.Red;
+                }
+            }
+            if (e.KeyCode == Keys.Up)
+            {
+                if (btnstart.ForeColor == System.Drawing.Color.Red)
+                {
+                    btnstart.ForeColor = System.Drawing.Color.Black;
+                    btnoptions.ForeColor = System.Drawing.Color.Red;
+                }
+                if (btnoptions.ForeColor == System.Drawing.Color.Red)
+                {
+                    btnoptions.ForeColor = System.Drawing.Color.Black;
+                    btnexit.ForeColor = System.Drawing.Color.Red;
+                }
+                if (btnexit.ForeColor == System.Drawing.Color.Red)
+                {
+                    btnexit.ForeColor = System.Drawing.Color.Black;
+                    btnstart.ForeColor = System.Drawing.Color.Red;
+                }
+            }*/
+            if (e.KeyCode == Keys.Enter)
+            {
+                if (btnstart.ForeColor == System.Drawing.Color.Red)
+                {
+                    btnstart.PerformClick();
+                }
+                if (btnoptions.ForeColor == System.Drawing.Color.Red)
+                {
+                    btnoptions.PerformClick();
+                }
+                if (btnexit.ForeColor == System.Drawing.Color.Red)
+                {
+                    btnexit.PerformClick();
+                }
+            }
+        }
+
+        private void png_Tick(object sender, EventArgs e)
+        {
+            pngtime++;
+            if (pngtime>=5)
+            {
+                girişpng.Visible = false;
+                png.Stop();
+            }
+        }
+
+        private void girişpng_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void sound_Tick(object sender, EventArgs e)
+        {
+            song("start");
+        }
+
+        public void song(string comedstring)
+        {
+            comed = comedstring.ToString();
+            /*foreach (string amk in comedstring)
+            {
+                comed = amk;
+            }*/
+            System.IO.Stream str = Properties.Resources.PederBHellandAlways;
+            System.Media.SoundPlayer snd = new System.Media.SoundPlayer(str);
+            if (comed == "start")
+            {
+                
+                snd.Play();
+            }
+            else if (comed == "stop")
+            {
+                snd.Stop();
+            }
+        }
+    }
+}
